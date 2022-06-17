a55b3ef8eca76669967d62a320be21b97bc24813
sanand100 <shraddhaanand1@gmail.com>
2022-06-17 08:21:37 -0400

    create and style (initial styling) Header component

---
+++ src/Components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css'
+function Header(props) {
+	return <header>
+        <h1>TravelTrips</h1>
+        <h4>Your only travel-planning destination</h4>
+    </header>;
+}
+
+export default Header;
---
+++ src/Components/HomeAndSearch.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function HomeAndSearch(props){
+    return <div></div>
+}
+
+export default HomeAndSearch
